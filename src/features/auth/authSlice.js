c5198343f33fbc48e8821603316edb41727bf84e
DanielMiguelez <danielmiguelez1993@gmail.com>
2023-01-24 11:43:04 +0100

    Register in process

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,22 +1,37 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import authService from "./authService";
+const user = JSON.parse(localStorage.getItem("user"));
 
 const initialState = {
-  user: null,
+  user: user ? user : null,
+  isError: false,
+  isSuccess: false,
+  message: "",
 };
-
-export const register = createAsyncThunk("auth/register", async (user) => {
-  try {
-    return await authService.register(user);
-  } catch (error) {
-    console.error(error);
-  }
-});
 
 export const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(register.fulfilled, (state, action) => {
+      state.isSuccess = true;
+
+      state.message = action.payload.message;
+    });
+  },
 });
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      const message = error.response.data.msg;
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export default authSlice.reducer;
