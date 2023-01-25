f3769ff760b12c285a122f8b63cff3ee73d2760a
SergioJ-Vidal <113844469+SergioJ-Vidal@users.noreply.github.com>
2023-01-25 16:57:51 +0100

    Categories Request Linked

--- src/components/Request/Category/Category.jsx
+++ src/components/Request/Category/Category.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
+import { useNavigate, Link } from "react-router-dom";
 import "./Category.css"
 
 const Category = () => {
+
+  const navigate = useNavigate();
+
   return (
+
     <div className='body-categories'>
 
       <span className='span-categories'>Selecciona la categoría relacionada con tu nueva petición:</span>
 
       <div className='all-boxes'>
+
         <div className='boxes-container1'>
-          <div className='category-box'>
+          <Link to="/requests/create/1" > <div className='category-box' ><img></img></div></Link>
 
-          </div>
-          <div className='category-box'>
+          <Link to="/requests/create/2" > <div className='category-box' ><img></img></div></Link>
 
-          </div>
-          <div className='category-box'>
-
-          </div>
+          <Link to="/requests/create/3" > <div className='category-box' ><img></img></div></Link>
         </div>
+
         <div className='boxes-container2'>
-          <div className='category-box'>
+          <Link to="/requests/create/4" > <div className='category-box' ><img></img></div></Link>
 
-          </div>
-          <div className='category-box'>
+          <Link to="/requests/create/5" > <div className='category-box' ><img></img></div></Link>
 
-          </div>
-          <div className='category-box'>
-
-          </div>
+          <Link to="/requests/create/6" > <div className='category-box' ><img></img></div></Link>
         </div>
+        
       </div>
     </div>
   )
